14aa81dec3cbe798ecc045ae621f04db957eb228
chaemin <w2114@e-mirim.hs.kr>
2023-03-10 11:53:00 +0900

    인라인 스타일링

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -2,8 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const number = undefined;
-  return <>{number || '리액트'}</>;
+  const name = '리액트';
+  const style1 = {
+    backgroundColor : 'black',
+    color:'aqua',
+    fontSize:'48px',
+    fontWeight:'bold',
+    padding : 16 //단위 px
+  };
+  return <div style={style1}>{name}</div>
 }
 
 export default App;
