1ded57aafb9d42b187be2bd2e747b7154d5b92ac
chaemin <w2114@e-mirim.hs.kr>
2023-04-21 11:05:28 +0900

    useMemo

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -1,7 +1,14 @@
-import Coonter from "./Counter";
+import { useState } from "react";
+import Timer from "./component/Timer";
 
 const App = () => {
-  return <Counter />;
+  const [showTimer, setShowTimer] = useState(false);
+  return(
+    <div>
+      {showTimer && <Timer/>}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle</button>
+    </div>
+  );
 };
 
 export default App;
