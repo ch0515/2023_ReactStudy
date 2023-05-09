f77be3796a7abca4c6ad20d6edcc1d5a06473e54
chaemin <w2114@e-mirim.hs.kr>
2023-05-09 14:59:39 +0900

    dom 요소 접근

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -1,17 +1,22 @@
-import React,{useState, useRef, useEffect} from "react";
+import React, {useRef, useEffect} from "react";
 
 const App = () => {
-  const [count, setCount] = useState(1);
-  const renderCount = useRef(1);
+  const inputRef = useRef();
 
   useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-    console.log("렌더링 수", renderCount.current);
-  });
-  return(
+    console.log(inputRef);
+    inputRef.current.focus();
+  }, []);
+  const login = () => {
+    alert(`환영합니다 ${inputRef.current.value}!`);
+    inputRef.current.value = ""; //input 박스의 내용을 삭제
+    inputRef.current.focus(); //input박스에 포커스
+  };
+  return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>올려</button>
+      <input ref={inputRef} type="text" placeholder="username"/>
+      <button onClick={login}>로그인</button>
     </div>
   );
 };
+export default App;
