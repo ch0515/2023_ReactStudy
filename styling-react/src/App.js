c886757c56dc34721b4968ae7bc045bd4db3a8bf
chaemin <w2114@e-mirim.hs.kr>
2023-05-30 14:44:01 +0900

    routerdom 설치

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,39 +1,12 @@
-import styled from "styled-components";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter} from "react-router-dom";
 
-const SimpleButton = styled.button`
-color: white;
-background-color: green;`;
-
-const LargeButton = styled(SimpleButton)`
-font-size: 50px;`;
-//일반적인 방법
-const ReactButton = (props) => {
-  console.log("props", props);
-  return <button className={props.className}>{props.children}</button>;
-};
-
-const ReactLargeButton = styled(ReactButton)`
-font-size: 50px;`;
-
-//primary라는 props를 주는 경우
-const PrimaryButton = styled.button`
-color: ${function (props){
-  if(props.primary){
-    return "white";
-  }else{
-    return "black";
-  }
-}};`;
-const App = () => {
-  return(
-    <div>
-      <SimpleButton>Simple</SimpleButton>
-      <LargeButton>Large</LargeButton>
-      <ReactButton>React</ReactButton>
-      <ReactLargeButton>React Large</ReactLargeButton>
-      <PrimaryButton>Normal</PrimaryButton>
-      <PrimaryButton>Primary</PrimaryButton>
-    </div>
-  );
-};
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+);
