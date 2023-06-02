8ef74b801cefed5c8b00d1f0c8edf52bd719b9dd
chaemin <w2114@e-mirim.hs.kr>
2023-06-02 10:58:05 +0900

    리액트 모듈

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/home";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>
-);
+const App = () => {
+  return(
+    <Route>
+      <Route Path="/" element={<Home/>}/>
+      <Route Path="/about" element={<About/>}/>
+    </Route>
+  );
+};
+
+export default App;
